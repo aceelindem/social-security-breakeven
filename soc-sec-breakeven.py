08d646c2fd994342ed66fb9c1d6c9ed8e12c3801
Acee Lindem <aceelindem@gmail.com>
2023-12-07 14:14:02 -0500

    Add defaults and summary statement.

--- soc-sec-breakeven.py
+++ soc-sec-breakeven.py
@@ -11,15 +11,18 @@ def calculate_break_even(monthly_earlier, monthly_later, interest_rate, months_e
     principal_later= 0
     month = 0
 
-    print(f"Year     Earlier Principle      Later Principal")
+    print(f"\nComparison of collecting ${monthly_earlier:.2f} {months_earlier} months earlier\
+ or ${monthly_later:.2f}  with an average interest rate of {interest_rate_decimal}%\n\n")
+    
+    print(f"Year     Earlier Principal      Later Principal")
     while principal_earlier >= principal_later:
         month += 1
         
-        # Compute early principle
+        # Compute early principal
         principal_earlier += monthly_earlier
         principal_earlier += principal_earlier * (interest_rate_decimal / 12)
 
-        # Compute principle at 70 years old (maximum ss retirement age) 
+        # Compute principal at 70 years old (maximum ss retirement age) 
         if month >= months_earlier:
             principal_later += monthly_later
             principal_later += principal_later * (interest_rate_decimal / 12)
@@ -31,10 +34,14 @@ def calculate_break_even(monthly_earlier, monthly_later, interest_rate, months_e
 
 def main(args):
 # Get inputs from the user
-    monthly_earlier = float(input("Enter the earlier monthly social security amount: "))
-    monthly_later = float(input("Enter the monthly later social security amount [Press Enter for default: $4850]): ") or "4850")
-    interest_rate = float(input("Enter the annual interest rate (in percent): "))
-    months_earlier = int(input("Enter the number of months earlier: "))
+    monthly_earlier = float(input(
+        "Enter the earlier monthly social security amount [Press Enter for default: $3741]: ")or "3741")
+    monthly_later = float(input(
+        "Enter the monthly later social security amount [Press Enter for default: $4850]): ") or "4850")
+    interest_rate = float(input(
+        "Enter the annual interest rate (in percent) [Press Enter for default: 0.0]: ") or "0.0")
+    months_earlier = int(input(
+        "Enter the number of months earlier [Press Enter for default: 40]: ") or "40")
 
     # Calculate and print the result
     result = calculate_break_even(monthly_earlier, monthly_later, interest_rate, months_earlier)
